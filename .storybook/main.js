b37ba7657e2d8400c82e641170cdae6e0ea6365c
Samuel MARLHENS <samuel.marlhens@gmail.com>
2021-07-13 20:02:51 +0200

    fix(storybook): build w/o postcss

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,10 @@
 const WindiCSS = require('windicss-webpack-plugin').default;
 
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+  features: {
+    postcss: false,
+  },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-svelte-csf'],
   svelteOptions: {
     preprocess: require('svelte-preprocess')(),
